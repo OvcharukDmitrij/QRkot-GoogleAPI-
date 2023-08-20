9ce194ce56d88a58a5d7637de85f94a52c694305
Дмитрий Овчарук <ovcharukdmitrij@yandex.ru>
2023-08-20 17:23:36 +0300

    tests and flake8 done

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router, project_router, donation_router
+from app.api.endpoints import (user_router, project_router,
+                               donation_router, google_api_router)
 
 main_router = APIRouter()
 main_router.include_router(
@@ -8,5 +9,8 @@ main_router.include_router(
 )
 main_router.include_router(
     donation_router, prefix='/donation', tags=['Donation']
+)
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
 )
 main_router.include_router(user_router)
--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List, Dict
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -24,6 +24,26 @@ class CRUDCharityProject(CRUDBase):
         db_project_id = db_project_id.scalars().first()
 
         return db_project_id
+
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession,
+    ) -> List[Dict[str, str]]:
+        close_projects = await session.execute(
+            select(CharityProject).where(CharityProject.fully_invested)
+        )
+        close_projects = close_projects.scalars().all()
+
+        close_projects_with_period = []
+
+        for project in close_projects:
+            close_projects_with_period.append({
+                'name': project.name,
+                'period': project.close_date - project.create_date,
+                'description': project.description,
+            })
+
+        return close_projects_with_period
 
 
 charityproject_crud = CRUDCharityProject(CharityProject)
